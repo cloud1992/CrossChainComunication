963760672b6d94159950ff199b8b18b0408a53b1
cloud1992 <arturocollado@ratherlabs.com>
2024-02-01 22:37:45 -0300

    :bug: fixed semicolon

--- src/Gateway/Gateway.sol
+++ src/Gateway/Gateway.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "./GatewayConfig.sol":;
+import "./GatewayConfig.sol";
 
 contract Gateway is GatewayConfig {
     receive() external payable {}
