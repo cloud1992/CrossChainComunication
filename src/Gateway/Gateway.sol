6c84cca8840e3017f8f091abcf4cb853e6b8d136
cloud1992 <arturocollado@ratherlabs.com>
2024-02-01 23:17:17 -0300

    :truck: set constructor

--- src/Gateway/Gateway.sol
+++ src/Gateway/Gateway.sol
@@ -6,6 +6,16 @@ import "./GatewayConfig.sol";
 contract Gateway is GatewayConfig {
     receive() external payable {}
 
+    constructor() {
+        __Ownable_init(msg.sender);
+    }
+
+    // onlyApproved modifier
+    modifier OnlyApproved() {
+        require(_onlyApproved[msg.sender], "Gateway: not approved");
+        _;
+    }
+
     // send function
     function send(
         uint dstChainId,
@@ -22,7 +32,7 @@ contract Gateway is GatewayConfig {
         address from,
         bytes calldata adapterParams,
         bytes calldata payload
-    ) internal override {
+    ) internal {
         // do something
     }
 }
--- src/Gateway/GatewayConfig.sol
+++ src/Gateway/GatewayConfig.sol
@@ -4,11 +4,6 @@ pragma solidity ^0.8.13;
 import "./GatewayStorage.sol";
 
 abstract contract GatewayConfig is GatewayStorage {
-    // init function
-    function initialize() external {
-        __Ownable_init();
-    }
-
     // set onlyApproved
     function setOnlyApproved(address _addr, bool _status) external onlyOwner {
         _onlyApproved[_addr] = _status;
